7ecf220928a411cc4727ff2db850aeb35501e9a4
CamooCodee <camo3519@gmail.com>
2026-01-15 14:10:22 +0100

    onedark

--- lua/core/plugin_config/colors.lua
+++ lua/core/plugin_config/colors.lua
@@ -1,6 +1,20 @@
-require('rose-pine').setup({
-  variant = 'moon',
-  dark_variant = 'moon'
+require('onedark').setup({
+    style = 'darker',
 })
 
-vim.cmd.colorscheme('rose-pine')
+vim.cmd.colorscheme('onedark')
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+
+-- Define the function globally
+_G.FixColors = function()
+    -- The highlight groups you want to clear
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+
+
+    print("Background transparency applied!")
+end
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -14,6 +14,7 @@ local packer_bootstrap = ensure_packer()
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use {'rose-pine/neovim', as = 'rose-pine'}
+    use {'navarasu/onedark.nvim', as = 'onedark'}
     use 'ThePrimeagen/vim-be-good'
     use 'nvim-treesitter/nvim-treesitter'
 	use 'nvim-treesitter/nvim-treesitter-context'
