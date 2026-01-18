05de152062d9a73fc11291c84d7df59c3b85d177
CamooCodee <camo3519@gmail.com>
2026-01-18 12:46:09 +0100

    fix finding hidden files

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,5 +1,10 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set("n", "<leader>fd", builtin.find_files)
+vim.keymap.set("n", "<leader>fd", function ()
+    builtin.find_files({
+        hidden = true,
+        file_ignore_patterns = { ".git/" }
+    });
+end)
 vim.keymap.set("n", "<leader>fg", builtin.live_grep)
 vim.keymap.set("n", "<leader>ff", function () 
     builtin.grep_string({search = vim.fn.input("Grep > ") });
