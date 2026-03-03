6e0816b6c3b1870d69eb8ae273c7da22a4f8bb5a
CamooCodee <camo3519@gmail.com>
2026-03-03 20:57:36 +0200

    oil and cleanup

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,13 +1,31 @@
-return {
-    "navarasu/onedark.nvim",
-    name = "onedark",
-    priority = 1000,
-    config = function()
-        require("onedark").setup({
-            style = "darker",
-            transparent = true,
-        })
+-- return {
+--     "navarasu/onedark.nvim",
+--     name = "onedark",
+--     priority = 1000,
+--     config = function()
+--         require("onedark").setup({
+--             style = "darker",
+--             transparent = true,
+--         })
+--
+--         vim.cmd.colorscheme("onedark")
+--     end
+-- }
+--
 
-        vim.cmd.colorscheme("onedark")
+return {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        transparent = true,
+        styles = {
+            sidebars = "transparent",
+            floats = "transparent",
+        },
+    },
+    config = function(_, opts)
+        require("tokyonight").setup(opts)
+        vim.cmd.colorscheme("tokyonight")
     end
 }
