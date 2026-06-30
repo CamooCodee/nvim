97cc81ab7029865ccf8dab49059e3899c74e7133
CamooCodee <camo3519@gmail.com>
2026-06-30 18:50:15 +0200

    proper filtering and SUPER FAST SEARCH NOW WOWWWWW

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,30 @@
+local ignore = {
+    ".git/",
+    "node_modules/",
+    ".venv/",
+    "__pycache__/",
+    ".DS_Store",
+    "uv.lock",
+    "tmp/",
+}
+
+local function rg_find_command()
+    local cmd = { "rg", "--files", "--hidden", "--no-ignore" }
+    for _, pat in ipairs(ignore) do
+        table.insert(cmd, "--glob")
+        table.insert(cmd, "!" .. pat)
+    end
+    return cmd
+end
+
+local function to_lua_patterns(list)
+    local out = {}
+    for _, pat in ipairs(list) do
+        table.insert(out, (pat:gsub("([%.%-])", "%%%1")))
+    end
+    return out
+end
+
 return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
@@ -8,31 +35,34 @@ return {
             function()
                 require("telescope.builtin").find_files({
                     hidden = true,
-                    no_ignore = true,
-                    file_ignore_patterns = { ".git/", "tmp/", "node_modules", ".venv", "__pycache__", ".DS_Store" }
+                    find_command = rg_find_command(),
                 })
-            end
+            end,
         },
         {
             "<leader>fg",
             function()
                 require("telescope.builtin").live_grep()
-            end
+            end,
         },
         {
             "<leader>ff",
             function()
                 require("telescope.builtin").grep_string()
-            end
-        }
-        --     {
-        --         "<leader>gr",
-        --         function()
-        --             require("telescope.builtin").lsp_references()
-        --         end
-        --     }
+            end,
+        },
+        -- {
+        --     "<leader>gr",
+        --     function()
+        --         require("telescope.builtin").lsp_references()
+        --     end,
+        -- },
     },
     config = function()
-        require("telescope").setup()
-    end
+        require("telescope").setup({
+            defaults = {
+                file_ignore_patterns = to_lua_patterns(ignore),
+            },
+        })
+    end,
 }
