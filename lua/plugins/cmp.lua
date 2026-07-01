c4921cb7e092d7849736f169e8d9cc12e34f6995
CamooCodee <camo3519@gmail.com>
2026-07-01 18:05:08 +0200

    nicer completion

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -7,21 +7,32 @@ return {
         "hrsh7th/cmp-path",
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
+        "onsails/lspkind.nvim"
     },
     config = function()
         local cmp = require('cmp')
 
-        vim.o.completeopt = 'menu,menuone,noselect'
+        vim.o.completeopt = 'menu,menuone,fuzzy'
+
+        local lspkind = require('lspkind')
 
         cmp.setup({
+            completion = {
+                completeopt = 'menu,menuone,noinsert',
+            },
             snippet = {
                 expand = function(args)
                     require('luasnip').lsp_expand(args.body)
                 end,
             },
+            window = {
+                completion = {
+                    max_height = 8,
+                },
+            },
             mapping = cmp.mapping.preset.insert({
                 ['<C-Space>'] = cmp.mapping.complete(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ['<Tab>'] = cmp.mapping.confirm({ select = true }),
                 ['<C-n>'] = cmp.mapping.select_next_item(),
                 ['<C-p>'] = cmp.mapping.select_prev_item(),
                 ['<C-e>'] = cmp.mapping.abort(),
@@ -31,6 +42,16 @@ return {
                 { name = 'buffer' },
                 { name = 'path' },
             },
+            formatting = {
+                fields = { 'abbr', 'icon', 'menu' },
+                format = lspkind.cmp_format({
+                    maxwidth = {
+                        menu = 50,
+                        abbr = 50,
+                    },
+                    ellipsis_char = '...',
+                })
+            }
         })
     end
 }
